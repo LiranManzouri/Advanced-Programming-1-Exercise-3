80fe84c4dcf4c54eb160ee3c1a0d896d89af7856
LiranManzouri <liranman77@gmail.com>
2022-09-01 02:20:52 +0300

    Added command 6 - only need to send the matrix to the client.

--- Command/ConfusionMatrixCommand.cpp
+++ Command/ConfusionMatrixCommand.cpp
@@ -6,6 +6,7 @@
 #include "ConfusionMatrixCommand.h"
 
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -13,114 +14,75 @@ void ConfusionMatrixCommand::execute() {
 
     CreateClassifiedFiles createClassifiedFiles(*k, classifiedTrainData, classifiedTrainData);
     vector<string> classifiedTypes = createClassifiedFiles.createClassified();
-    vector<string> realTypes;
 
     string types = classifiedTrainData;
-    cout << types << endl;
-//    while (!types.empty()) {
-//
-//    }
+    vector<string> realTypes;
 
-    int counterForIrisSetosa = 0;
-    int counterForIrisVersicolor = 0;
-    int counterForIrisVirginica = 0;
+    // cout << types << endl;
+    string line;
+    char delim = ',';
+    while (!types.empty()) {
+        line = types.substr(0, types.find('\n'));
+        while (line.find(delim) != string::npos) {
+            line.erase(0, line.find(delim) + 1);
+        }
+        realTypes.push_back(line);
+        types.erase(0, types.find('\n') + 1);
+    }
 
-//    for (int i = 0; i < ; ++i) {
-//
-//    }
+    // for (auto &type : realTypes) {
+    //     cout << type << endl;
+    // }
+
+    double matrix[3][3] = {0};
+
+    for (int i = 0; i < realTypes.size(); i++) {
+        if (realTypes.at(i) == classifiedTypes.at(i) && realTypes.at(i) == "Iris-setosa") {
+            matrix[0][0]++;
+        } else if (realTypes.at(i) == classifiedTypes.at(i) && realTypes.at(i) == "Iris-versicolor") {
+            matrix[1][1]++;
+        } else if (realTypes.at(i) == classifiedTypes.at(i) && realTypes.at(i) == "Iris-virginica") {
+            matrix[2][2]++;
+        } else if (realTypes.at(i) == "Iris-setosa" && classifiedTypes.at(i) == "Iris-versicolor") {
+            matrix[0][1]++;
+        } else if (realTypes.at(i) == "Iris-setosa" && classifiedTypes.at(i) == "Iris-virginica") {
+            matrix[0][2]++;
+        } else if (realTypes.at(i) == "Iris-versicolor" && classifiedTypes.at(i) == "Iris-setosa") {
+            matrix[1][0]++;
+        } else if (realTypes.at(i) == "Iris-versicolor" && classifiedTypes.at(i) == "Iris-virginica") {
+            matrix[1][2]++;
+        } else if (realTypes.at(i) == "Iris-virginica" && classifiedTypes.at(i) == "Iris-setosa") {
+            matrix[2][0]++;
+        } else if (realTypes.at(i) == "Iris-virginica" && classifiedTypes.at(i) == "Iris-versicolor") {
+            matrix[2][1]++;
+        }
+    }
+
+    double percentsMatrix[3][3] = {0};
+    for (int j = 0; j < 3; j++) {
+        double total = matrix[j][0] + matrix[j][1] + matrix[j][2];
+        for (int l = 0; l < 3; l++) {
+            percentsMatrix[j][l] = round((matrix[j][l] / total) * 100);
+        }
+    }
+
+    string typesArray[3] = {"Iris-setosa", "Iris-versicolor", "Iris-virginica"};
+    for (int j = 0; j < 3; j++) {
+        cout << typesArray[j] << '\t';
+        for (int l = 0; l < 3; l++) {
+            cout << percentsMatrix[j][l] << "%" << '\t';
+        }
+        cout << endl;
+    }
+    cout << '\t' << typesArray[0] << '\t' << typesArray[1] + '\t' << typesArray[2] << endl;
 
 
+    /*                0             1               2
+     *              setosa      versicolor      virginica
+     *0 setosa        100%           0%             0%
+     *
+     *1 versicolor     0%           100%            0%
+     *
+     *2 virginica      0%            7%            93%
+     */
 }
-//    vector<string> columnsClassifiedTypes = *flowerTypes;
-////    string columnClassifiedTypes = classifiedTrainData;
-//
-//    string line = classifiedTrainData;
-//    vector<string> rowsRealTypes;
-//    while (!line.empty()) {
-//        string type = line.substr(0, line.find('\n'));
-//        rowsRealTypes.push_back(type);
-//        line.erase(0, line.find('\n') + 1);
-//    }
-//
-//    int l = 1;
-//    for (auto &type: rowsRealTypes) {
-//        cout << l++ << " " << type << endl;
-//    }
-//
-//
-//
-////    flowerType = line.substr(0, line.find('\r'));
-//
-//
-//
-//    int matrix[3][3];
-//
-//    /*                0             1               2
-//     *              setosa      versicolor      virginica
-//     *0 setosa        100%           0%             0%
-//     *
-//     *1 versicolor     0%           100%            0%
-//     *
-//     *2 virginica      0%            0%            100%
-//     */
-//
-//    for (int i = 0; i < rowsRealTypes.size(); ++i) {
-////        int setosaType = 0;
-////        int versicolorType = 0;
-////        int virginicaType = 0;
-//        int totalSum = 0;
-//        int realTypeCounter = 0;
-//        int firstWrongTypeCounter = 0;
-//        int secondWrongTypeCounter = 0;
-//        for (int j = 0; j < columnsClassifiedTypes.size(); ++j) {
-//            // Check if the real type is really what it is in the unclassified.csv
-//            if (rowsRealTypes.at(i) == columnsClassifiedTypes.at(j)) {
-//                realTypeCounter++;
-//            } else if (i == 0 && columnsClassifiedTypes.at(j) == "Iris-Versicolor") {
-//                firstWrongTypeCounter++;
-//            } else if (i == 0 && columnsClassifiedTypes.at(j) == "Iris-Virginica") {
-//                secondWrongTypeCounter++;
-//            } else if (i == 1 && columnsClassifiedTypes.at(j) == "Iris-Setosa") {
-//                firstWrongTypeCounter++;
-//            } else if (i == 1 && columnsClassifiedTypes.at(j) == "Iris-Virginica") {
-//                secondWrongTypeCounter++;
-//            } else if (i == 2 && columnsClassifiedTypes.at(j) == "Iris-Setosa") {
-//                firstWrongTypeCounter++;
-//            } else if (i == 2 && columnsClassifiedTypes.at(j) == "Iris-Versicolor") {
-//                secondWrongTypeCounter++;
-//            }
-//            totalSum = realTypeCounter + firstWrongTypeCounter + secondWrongTypeCounter;
-//            cout << totalSum << endl;
-//        }
-//    }
-////            if (i == j && rowsRealTypes.at(i) == columnsClassifiedTypes.at(j)) {
-////                realTypeCounter++;
-////            }
-////            if (i == 0 && j == 1 && rowsRealTypes.at(i) != columnsClassifiedTypes.at(j)) {
-////                firstWrongTypeCounter++;
-////            }
-////            if (i == 0 && j == 2 && rowsRealTypes.at(i) != columnsClassifiedTypes.at(j)) {
-////                secondWrongTypeCounter++;
-////            }
-////            if (i == 1 & j == 0 && rowsRealTypes.at(i) != columnsClassifiedTypes.at(j)) {
-////                firstWrongTypeCounter++;
-////            }
-////            if (i == 1 & j == 1 && rowsRealTypes.at(i) != columnsClassifiedTypes.at(j)) {
-////                secondWrongTypeCounter++;
-////            }
-////            if (i == 2 & j == 0 && rowsRealTypes.at(i) != columnsClassifiedTypes.at(j)) {
-////                firstWrongTypeCounter++;
-////            }
-////            if (i == 2 & j == 1 && rowsRealTypes.at(i) != columnsClassifiedTypes.at(j)) {
-////                secondWrongTypeCounter++;
-////            }
-//
-////            if (i == j - 2 || i == j + 2) { secondWrongTypeCounter++; }
-////        }
-////    }
-//
-////    cout << classifiedTrainData << endl;
-//
-//
-//
-//}
